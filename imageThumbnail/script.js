abfefec2e8edccfae394c4930d4b8aa8411f456c
Bui Duy <nd26041998@gmail.com>
2017-10-23 14:11:25 +0700

    create ratingbase on the data from DB

--- imageThumbnail/script.js
+++ imageThumbnail/script.js
@@ -1,1 +1,9 @@
-
+var rating = 4;
+for (i = 1; i <= 5; i++) {
+    var targetStar = document.getElementById("star" + (6 - i));
+    if (i <= rating) {
+        targetStar.innerHTML = "&starf;";
+    } else {
+        targetStar.innerHTML = "☆";
+    }
+}
